9250bb470ebb4ddd17db8bc11d6e915a2d12c075
Thomas Chula <thomaschula@gmail.com>
2025-10-01 09:12:24 +0300

    Randomly pick between Rock, paper and Scissors
    Updated the js file to randomly pick Rock, Paper
    and Scissor using Math.random

--- rps.js
+++ rps.js
@@ -1,1 +1,15 @@
-console.log('Hello World');
+function getComputerChoice() {
+    let picked = Math.random();
+    if(picked >= 0 && picked <= 0.33) {
+        selected = "Rock";
+    } else if(picked > 0.33 && picked <= 0.67) {
+        selected = "Paper";
+    } else {
+        selected = "Scissor";
+    }
+    // return picked;
+    return selected;
+    }
+
+    console.log(getComputerChoice());
+// console.log('Hello World');
